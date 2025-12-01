f4804b66b20b39c41a706197c4e2c9ed7311e379
Arpit Tripathi <arpit.tripathi0610@gmail.com>
2025-12-01 22:55:43 +0530

    new routes fro user events

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { register,login,verifyUser,resendOtp,resetPassword,forgotPassword} from '../controllers/authController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = Router();
 
@@ -7,7 +8,8 @@ router.post('/register', register);
 router.post('/login', login);
 router.post('/verify', verifyUser);
 router.post('/resend', resendOtp);
-router.post('/forgot-password',forgotPassword)
-router.post('/reset-password',resetPassword)
+router.post('/forgot-password',forgotPassword);
+router.post('/reset-password',resetPassword);
+router.put('/profile', protect, updateUserProfile);
 
 export default router;
